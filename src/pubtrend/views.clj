d873e55bf1737ce46908b23366188c15cb2305a1
Alex Kalderimis <alexkalderimis@gmail.com>
2013-10-27 15:02:08 +0000

    Serve a page with a require.js entrypoint

--- src/pubtrend/views.clj
+++ src/pubtrend/views.clj
@@ -11,20 +11,25 @@
 (defn- with-utf8-charset [[tag attrs]]
   [tag (assoc attrs :charset "utf-8")])
 
-(defn- num-field [attr-map name &[ min-val max-val value ]]
+(def ^:dynamic require-js "/vendor/requirejs/require.js")
+
+(defn- entry-point [ep]
+  (->> (include-js require-js)
+       (map (fn [[tag attrs]] [tag (assoc attrs :data-main ep)]))
+       (first)))
+
+(defn- num-field [id &[ min-val max-val value ]]
   (let [assoc-if (fn [m k v] (if (nil? v) m (assoc m k v)))
-        attrs (-> (assoc attr-map :type "number")
+        attrs (-> {:type "number" :id id}
                   (assoc-if :min min-val)
                   (assoc-if :max max-val)
                   (assoc-if :value value))]
     [:input attrs]))
 
 (def vendor-scripts [
-;;                     "/vendor/requirejs/require.js"
                      "/vendor/zepto/zepto.js"
                      "/vendor/underscore/underscore-min.js"
                      "/vendor/backbone/backbone-min.js"
-                     "/vendor/q/q.js"
                      "/js/d3.v3.js"])
 
 (defn common [title & body]
@@ -37,7 +42,7 @@
      (include-css "/stylesheets/style.css"
                   "/stylesheets/foundation-4.3.2/normalize.css"
                   "/stylesheets/foundation-4.3.2/foundation.min.css")
-     (include-css "http://fonts.googleapis.com/css?family=Sigmar+One&v1")]
+     (include-css "http://fonts.googleapis.com/css?family=Rokkitt")]
     [:body
      [:nav {:class "top-bar hide-for-small"}
       [:ul {:class "title-area"}
@@ -56,24 +61,36 @@
      [:div {:class "large-3 columns"}
       [:div {:class "callout panel"}
        [:h3 "Trend Parameters"]
-       [:p "Adjust the trend parameters here"]]]
+       [:p "Adjust the trend parameters here. You can search for one or
+            more terms over a given period, up to the present day. Change
+            the displayed window of time by using the pagination controls,
+            or the left and right buttons on the keyboard."]]]
      [:div {:class "large-9 columns"}
       [:form {:id "pubtrend-terms"}
        [:fieldset
         [:legend "Parameters"]
         [:div {:class "row"}
+         
          [:div {:class "large-3 columns"}
           (form/label "pubtrends-term" "Terms")
           [:div {:id "pubtrends-terms"}
-           (form/text-field {:class "pubtrends-term"} "term")]]
-          [:button { :title "Add a search term" :id "add-term"
-                    :class "small secondary button"} "+"]]
+           (form/text-field {:class "pubtrends-term"} "term")]
+          [:div {:class "row"}
+           [:div {:class "small-6 columns"}
+            [:button { :title "Add a search term" :id "add-term"
+                      :class "small secondary button"} "+"]]
+           [:div {:class "small-6 columns"}
+            [:button { :title "Remove the last search term" :id "rem-term"
+                      :class "small secondary button"} "-"]]]]
+
          [:div {:class "large-3 columns"}
           (form/label "pubtrends-from" "Start")
-          (num-field {:id "pubtrends-from"} 1800 this-year (- this-year 20))
+          (num-field "pubtrends-from" 1800 this-year (- this-year 20))]
+
          [:div {:class "large-3 columns"}
           (form/label "pubtrends-til" "End")
-          (num-field {:id "pubtrends-til"} 1800 this-year this-year)]
+          (num-field "pubtrends-til" 1800 this-year this-year)]
+
          [:div {:class "large-3 columns"}
           (form/submit-button {:class "button"} "get-data")]]]]]]))
 
@@ -96,5 +113,5 @@
           pagination
           (trend-controls)
           (map with-utf8-charset
-               (apply include-js (conj vendor-scripts "/js/pubtrends.js")))))
-
+               (conj (apply include-js vendor-scripts)
+                     (entry-point "/js/main")))))
