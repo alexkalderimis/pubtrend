fe4d64a19a62544bfbfc630bf8d4f410f0a29be0
Alex Kalderimis <alexkalderimis@gmail.com>
2013-10-27 16:42:17 +0000

    Added server-less static version.
    This demonstrates modularity and full capabilities of client side app.

---
+++ resources/public/js/static.js
@@ -0,0 +1,33 @@
+require.config({
+  paths: {
+    "Q": "/resources/public/vendor/q/q",
+    "data-source": "/resources/public/js/client-side-data-source"
+  }
+});
+
+require(["./trend-view", "./trend-chart-view", "./dispatcher"],
+  function (TrendView, TrendChartView, dispatcher) {
+    
+  var CURRENT_YEAR = (new Date()).getUTCFullYear();
+  var model = new Backbone.Model();
+  var main = function(opts) {
+    ["earlier", "later"].forEach(function(direction) {
+      $('#show-' + direction).click(function () {
+        dispatcher.trigger("page-chart", direction);
+      });
+    });
+    var view = new TrendView({model: model})
+      , chartView = new TrendChartView({model: model})
+      , chartElem = document.getElementById('pubtrend-viz')
+      , controlElem = document.getElementById('pubtrend-terms');
+    chartView.setElement(chartElem);
+    view.setElement(controlElem);
+    model.set(opts);
+  };
+
+  main({
+    terms: ["asthma"],
+    start: CURRENT_YEAR - 20,
+    end: CURRENT_YEAR
+  });
+});
