21f3110e010d23750e755cf7ea7a21767df9f9c5
Alex Kalderimis <alexkalderimis@gmail.com>
2013-10-31 09:59:02 +0000

    Don't alter input - reject it.

--- resources/public/js/trend.js
+++ resources/public/js/trend.js
@@ -3,26 +3,19 @@ define(['./dispatcher'], function (dispatcher) {
   var Trend = Backbone.Model.extend({
 
     set: function(attrs, opts) {
-      var start, userStart, userEnd;
+      var start, end;
       if (typeof arguments[0] === 'string') {
         attrs = {};
         attrs[arguments[0]] = arguments[1];
         opts = arguments[2];
       }
-      userStart = attrs.start;
-      userEnd = attrs.end;
-      if (attrs.start != null) {
-        attrs.start = Math.max(Trend.MIN, attrs.start);
-        attrs.start = Math.min(Trend.MAX, attrs.start);
-      }
-      if (attrs.end != null) {
-        attrs.end = Math.max(Trend.MIN, attrs.end);
-        attrs.end = Math.min(Trend.MAX, attrs.end);
-        start = (attrs.start == null) ? this.attributes.start : attrs.start;
-        attrs.end = Math.max(start, attrs.end);
-      }
-      if (attrs.start !== userStart || attrs.end !== userEnd) {
-        dispatcher.trigger("Trend:rejected-values", this, userStart, userEnd);
+      start = (attrs.start || this.attributes.start);
+      end = (attrs.end || this.attributes.end);
+
+      if (start < Trend.MIN || start > end || start > Trend.MAX
+         || end > Trend.MAX || end < start || end < Trend.MIN) {
+        this.trigger("reject-values", this, start, end);
+        return dispatcher.trigger("Trend:rejected-values", this, start, end);
       }
       return Backbone.Model.prototype.set.call(this, attrs, opts);
     }
