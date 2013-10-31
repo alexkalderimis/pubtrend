b22de5efc7c40194aefae7b8679fe3a39915027d
Alex Kalderimis <alexkalderimis@gmail.com>
2013-10-31 09:57:59 +0000

    Added events for zooming and rescaling

--- resources/public/js/main.js
+++ resources/public/js/main.js
@@ -3,7 +3,7 @@
 define(["./trend", "./trend-view", "./trend-chart-view", "./dispatcher", "./messages"],
   function (Trend, TrendView, TrendChartView, dispatcher, Messages) {
 
-  var msgTemp = _.template("Invalid range. using <%= start %> - <%= end %>");
+  var msgTemp = _.template("Invalid range: <%= start %> - <%= end %>");
     
   /**
    * Main entry point. Instantiates the moving parts and gets the ball rolling.
@@ -23,9 +23,29 @@ define(["./trend", "./trend-view", "./trend-chart-view", "./dispatcher", "./mess
     chartView.setElement(chartElem);
     view.setElement(controlElem);
 
-    dispatcher.on("Trend:rejected-values", function (m, userStart, userEnd) {
-      var msg = msgTemp(m.toJSON());
+    dispatcher.on("Trend:rejected-values", function (m, start, end) {
+      var msg = msgTemp({start: start, end: end});
       messages.flash(msg);
+    });
+    // Simple global events and messages.
+    $('#instructions h3').click(function() {
+      $('#instructions p').fadeToggle();
+    });
+    $('#re-zero').click(function (evt) {
+      dispatcher.trigger("rescale-y");
+    });
+    ['in', 'out'].forEach(function(direction) {
+      var command = 'zoom-' + direction
+        , elemId = '#' + command;
+        $(elemId).click(function (evt) {
+          var state = model.toJSON()
+            , width = state.end - state.start
+            , delta = Math.floor((direction === 'out') ? (width / 2) : (-width / 3));
+          console.log(delta);
+          state.start = Math.max(Trend.MIN, state.start - delta);
+          state.end = Math.min(Trend.MAX, state.end + delta);
+          model.set(state);
+        });
     });
     ["earlier", "later"].forEach(function(direction) {
       $('#show-' + direction).click(function () {
