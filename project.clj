fde79dcc57f41a5b6b073eb4003af20311701605
Alex Kalderimis <alexkalderimis@gmail.com>
2013-11-09 01:17:31 +0000

    Moved clojure to own src directory

--- project.clj
+++ project.clj
@@ -6,10 +6,12 @@
                  [compojure "1.1.5"]
                  [hiccup "1.0.4"]
                  [org.clojure/java.jdbc "0.3.0-alpha5"]
+                 [dieter "0.4.1"]
                  [org.xerial/sqlite-jdbc "3.7.2"]
                  [cheshire "5.2.0"]
                  [org.slf4j/slf4j-simple "1.7.5"]
                  [http.async.client "0.5.2"]]
   :plugins [[lein-ring "0.8.5"]]
+  :source-paths ["src/clojure"]
   :ring {:handler pubtrend.handler/app}
   :profiles {:dev {:dependencies [[ring-mock "0.1.5"]]}})
